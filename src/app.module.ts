e5e4f6866afa4d4fdba9d989ee9dfb8155621886
algodoncin <179851883+algodondev@users.noreply.github.com>
2026-06-16 16:21:21 -0600

    feat: add users services and auth files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ServicesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
